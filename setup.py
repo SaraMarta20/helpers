b87901c6b9079ae5a7939d06ad4c306677893c8c
SaraMarta20 <sara.marta@outlook.fr>
2025-01-30 14:24:38 +0100

    Commit 1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+requirements = ["toml", "PyYAML"]
+
+setup(
+    name="helpers",
+    version="1.0",
+    install_requires=requirements,
+    packages=find_packages(),
+    author="Sara Moutinho Marta",
+)
